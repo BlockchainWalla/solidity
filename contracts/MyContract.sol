ddcb81229cc654c22698136726de99fb70e4f51d
BlockchainWalla <niteshtiwari89@icloud.com>
2024-02-11 18:48:51 +0000

    2ndCommit

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 contract MyContract{
-    uint public myUint = 123;
+    uint public myUint = 1234;
     function setMyUint(uint newUint) public{
         myUint=newUint;
     }
